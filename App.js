f64e9429c301f248b0e793eee84dd448b1cd6dac
Li Gang <gang.li@thoughtworks.com>
2021-09-16 22:44:57 +0800

    add flat list

--- App.js
+++ App.js
@@ -8,51 +8,53 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import HomeScreen from './src/HomeScreen';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        flex: 1
+    };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <HomeScreen />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            />
+            <HomeScreen />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600'
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400'
+    },
+    highlight: {
+        fontWeight: '700'
+    }
 });
 
 export default App;
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,46 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Colors from './Assets/Colors';
 
 const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        backgroundColor: '#eeeeee',
+        backgroundColor: Colors.white,
         alignItems: 'center',
         justifyContent: 'center'
-    },
-    view: {
-        backgroundColor: 'red'
     }
 });
+
+const DATA = [
+    {
+        title: 'title1',
+        description: 'afafjakldfajdfkjas'
+    },
+    {
+        title: 'title2',
+        description: 'afafjakldfajdfkjas'
+    },
+    {
+        title: 'title3',
+        description: 'afafjakldfajdfkjas'
+    },
+    {
+        title: 'title4',
+        description: 'afafjakldfajdfkjas'
+    },
+    {
+        title: 'title5',
+        description: 'afafjakldfajdfkjas'
+    }
+];
 
 class HomeScreen extends Component {
     constructor(props) {
         super(props);
-        this.title = 'React Native Workshop';
-
-        this.state = {
-            titleLabelColor: 'red'
-        };
     }
 
-    onButtonPressed = () => {
-        this.setState({
-            titleLabelColor: 'yellow'
-        });
+    renderItem = (item) => {
+        return (
+            <View
+                style={{
+                    width: '100%',
+                    height: 60,
+                    backgroundColor: Colors.cardBg,
+                    padding: 20
+                }}
+            >
+                <Text>{item.title}</Text>
+                <Text>{item.description}</Text>
+            </View>
+        );
     };
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={{ color: this.state.titleLabelColor }}>
-                    {this.title}
-                </Text>
-                <Button
-                    title="button"
-                    onPress={() => {
-                        this.onButtonPressed();
+                <FlatList
+                    style={{
+                        width: '100%'
                     }}
+                    data={DATA}
+                    renderItem={({ item }) => this.renderItem(item)}
                 />
             </View>
         );
