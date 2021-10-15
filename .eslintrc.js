df06d09b279ef6ce67be79b73def0ab1cff206a2
Li Gang <gang.li@thoughtworks.com>
2021-10-15 17:21:21 +0800

    Add Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 13,
+        sourceType: 'module'
+    },
+    plugins: ['react', 'react-native'],
+    rules: {
+        'no-undef': 'off'
+    }
 };
